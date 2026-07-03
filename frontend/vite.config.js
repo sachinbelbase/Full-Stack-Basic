557b5931681839f790605ed1e5ef86722e6b8864
sachinbelbase <sachinbelbase78@gmail.com>
2026-07-03 17:17:34 +0545

    change in server path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:5000/",
+      "/api": "https://full-stack-basic.netlify.app/",
     },
   },
   plugins: [react()],
