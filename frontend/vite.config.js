726b52eeee41f2abb5682d19cbc8372006302a81
sachinbelbase <sachinbelbase78@gmail.com>
2026-07-03 17:20:05 +0545

    cahnges in server path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://full-stack-basic.netlify.app/",
+      "/api": "https://full-stack-basic.vercel.app/api/jokes",
     },
   },
   plugins: [react()],
